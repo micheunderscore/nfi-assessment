6aa1df53ac4915d81dd98c0374ebc06533d6bc2b
Miche_ <51939599+micheunderscore@users.noreply.github.com>
2022-09-12 15:30:25 +0800

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { ethers } from "ethers";
+import React, { useState } from "react";
+import "./App.css";
+import chainIds from "./chainIds.json";
 
-function App() {
+const App = () => {
+  const [errorMessage, setErrorMessage] = useState("");
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [userBalance, setUserBalance] = useState("");
+  const [loading, setLoading] = useState({ address: false, balance: false });
+  const [userChainId, setUserChainId] = useState("");
+
+  const connectWalletHandler = () => {
+    if (window.ethereum) {
+      // metamask exists
+      setLoading({ ...loading, address: true });
+      window.ethereum
+        .request?.({
+          method: "eth_requestAccounts",
+        })
+        .then((result) => {
+          accountChangedHandler(result[0]);
+        });
+    } else {
+      setErrorMessage("Please Install MetaMask");
+    }
+  };
+
+  const accountChangedHandler = (newAccount) => {
+    setDefaultAccount(newAccount.toString());
+    getUserBalance(newAccount);
+    getChainId();
+    setLoading({ ...loading, address: false });
+  };
+
+  const getUserBalance = (address) => {
+    setLoading({ ...loading, balance: true });
+    window.ethereum
+      ?.request?.({ method: "eth_getBalance", params: [address, "latest"] })
+      .then((balance) => {
+        setUserBalance(ethers.utils.formatEther(balance));
+        setLoading({ ...loading, balance: false });
+      });
+  };
+
+  const getChainId = () => {
+    window.ethereum
+      ?.request?.({ method: "eth_chainId" })
+      .then((chainId) => setUserChainId(chainIds[chainId].network));
+  };
+
+  const chainChangedHandler = () => {
+    window.location.reload();
+  };
+
+  window.ethereum?.on?.("accountsChanged", accountChangedHandler);
+
+  window.ethereum?.on?.("chainChanged", chainChangedHandler);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Connection to MetaMask using window.ethereum method</h4>
+      <button onClick={connectWalletHandler}>Connect Wallet</button>
+      <div className="accountDisplay">
+        {!loading.address ? (
+          <h3>Address: {defaultAccount}</h3>
+        ) : (
+          <div className="loader"></div>
+        )}
+      </div>
+      <div className="balanceDisplay">
+        {!loading.balance ? (
+          <h3>Balance: {userBalance}</h3>
+        ) : (
+          <div className="loader"></div>
+        )}
+      </div>
+      <div className="chainDisplay">
+        <h3>Network: {userChainId}</h3>
+      </div>
+      {errorMessage}
     </div>
   );
-}
+};
 
 export default App;
